e17f9de2fed55ee0f77039f92c15f2a771d2b799
sgupta98mnit <sgupta98mnit@gmail.com>
2021-05-04 14:29:54 +0530

    Added timestamps for messages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
-
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+ 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -12,28 +13,26 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
     console.log('New WebSocket Connection!');
     // socket.emit emits to a pirticular socket whereas io.emit emits to all socket
 
-    socket.emit('message', 'Welcome!!');
+    socket.emit('message', generateMessage('Welcome!'));
 
-    socket.broadcast.emit('message', 'A new user has joined!');
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
     socket.on('sendMessage', (message, callback) => {
-        io.emit('message', message);
+        io.emit('message', generateMessage(message));
         callback();
     });
 
     socket.on('sendLocation', (location, callback) => {
-        io.emit('locationMessage', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.latitude},${location.longitude}`));
         callback();
     });
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has disconnected.')
+        io.emit('message', generateMessage('A user has disconnected.'))
     });
 
 })
